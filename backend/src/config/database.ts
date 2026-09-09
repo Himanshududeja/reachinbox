5fc6f48fc0901de2851601dc99c8d78fa9eb035c
Himanshu <dudejahim123@gmail.com>
2026-09-09 15:07:38 +0530

    fix production database connection

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,13 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  socketPath: process.env.DB_SOCKET,
+const config: any = {
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10
-});
+};
+
+if (process.env.DB_SOCKET) {
+  config.socketPath = process.env.DB_SOCKET;
+}
+
+const pool = mysql.createPool(config);
 
 export default pool;
